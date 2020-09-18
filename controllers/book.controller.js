067bee18cfcd1f024247e2871655e121f7bea7aa
edindzabija <edindzabija@gmail.com>
2020-09-19 00:18:39 +0200

    update book

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -15,9 +15,23 @@ exports.book_create = (req, res) => {
   });
 };
 
+exports.all_books = (req, res) => {
+  Book.find({}, (err, book) => {
+    if (err) return next(err);
+    res.json(book);
+  });
+};
+
 exports.book_details = (req, res) => {
   Book.findById(req.params.id, (err, book) => {
     if (err) return next(err);
     res.send(book);
+  });
+};
+
+exports.book_update = (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, book) => {
+    if (err) return next(err);
+    res.send("Book Udpated.");
   });
 };
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const book_controller = require("../controllers/book.controller");
 
-router.post("/create", book_controller.book_create);
+router.get("/", book_controller.all_books);
 
 router.get("/:id", book_controller.book_details);
+
+router.post("/create", book_controller.book_create);
+
+router.put("/:id/update", book_controller.book_update);
 
 module.exports = router;
