668c26c64e2687941d8ffba66f9f750085712607
edindzabija <edindzabija@gmail.com>
2020-09-23 15:10:35 +0200

    minor fix

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,6 +1,6 @@
 const Book = require("../models/book.model");
 
-exports.book_create = (req, res) => {
+exports.book_create = (req, res, next) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
@@ -15,28 +15,28 @@ exports.book_create = (req, res) => {
   });
 };
 
-exports.all_books = (req, res) => {
+exports.all_books = (req, res, next) => {
   Book.find({}, (err, book) => {
     if (err) return next(err);
     res.json(book);
   });
 };
 
-exports.book_details = (req, res) => {
+exports.book_details = (req, res, next) => {
   Book.findById(req.params.id, (err, book) => {
     if (err) return next(err);
     res.send(book);
   });
 };
 
-exports.book_update = (req, res) => {
+exports.book_update = (req, res, next) => {
   Book.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, book) => {
     if (err) return next(err);
     res.send("Book Udpated.");
   });
 };
 
-exports.book_delete = (req, res) => {
+exports.book_delete = (req, res, next) => {
   Book.findByIdAndRemove(req.params.id, (err) => {
     if (err) return next(err);
     res.send("Book Deleted");
